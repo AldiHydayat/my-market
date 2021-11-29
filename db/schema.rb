9e43047ba2b2bcff0e9b2f92d20ca7b8d12b6bc1
Aldi Hidayat <aldyh128@gmail.com>
2021-11-29 09:02:04 +0700

    menambah table categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_015135) do
+ActiveRecord::Schema.define(version: 2021_11_29_020020) do
+
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
