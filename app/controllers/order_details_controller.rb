df9829153773a6a89ad01e51ed887f306f8e4319
Aldi Hidayat <aldyh128@gmail.com>
2021-12-10 15:23:04 +0700

    memperbaiki summary view dan update user

--- app/controllers/order_details_controller.rb
+++ app/controllers/order_details_controller.rb
@@ -2,6 +2,7 @@ class OrderDetailsController < ApplicationController
   before_action :authenticate_user!
 
   def summary
+    @order_detail = OrderDetail.find(params[:id])
     @summary = OrderDetail.get_product_summary(params[:id])
   end
 end
